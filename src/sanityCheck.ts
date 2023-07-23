d17f117d9eaaeb848f5005552b60a5c1ded29bf1
gimpey <gimpey+github@pr0xy.io>
2023-07-23 17:45:25 -0400

    moved the sanity check

--- src/sanityCheck.ts
+++ src/sanityCheck.ts
@@ -1,24 +1,23 @@
+import Alchemy from "./services/Alchemy";
+import logger from "./services/logger";
+import upload from "./create/upload";
 import axios from "axios";
-import Alchemy from "../services/Alchemy";
 import _ from "lodash";
-import upload from "../create/upload";
-
-require("dotenv").config();
 
 /**
  * @title Sanity Check
- * @description The sanity check ensures existing tokens are uploaded once the program
- * is started. If the program goes down or misses a token, restarting it will upload
- * all previously minted tokens.
+ * @description The sanity check ensures existing tokens are uploaded once the
+ * program is started. If the program goes down or misses a token, restarting
+ * it will upload all previously minted tokens.
  */
 const sanityCheck = async () => {
-  console.log(`Performing sanity check for already minted tokens.`);
+  logger.info(`Performing sanity check for already minted tokens.`);
 
   const { contract } = Alchemy;
 
   let totalSupply = await contract.methods.totalSupply().call();
 
-  console.log(`${totalSupply} tokens have been minted so far.`);
+  logger.info(`${totalSupply} tokens have been minted so far.`);
 
   let tokenId = 0;
   while (totalSupply > 0) {
@@ -28,7 +27,7 @@ const sanityCheck = async () => {
       const metadata = require(`../../metadata/${tokenId}.json`);
 
       if (!_.isEqual(metadata, uploaded)) {
-        console.log(`Metadata for token ${tokenId} is out of sync, uploading.`);
+        logger.info(`Metadata for token ${tokenId} is out of sync, uploading.`);
         await upload(tokenId, metadata);
       }
 
@@ -36,7 +35,7 @@ const sanityCheck = async () => {
       tokenId++;
     } catch (error) {
       if (error.message.includes("execution reverted")) {
-        console.log(`${tokenId} does not exist, skipping sanity check.`);
+        logger.info(`${tokenId} does not exist, skipping sanity check.`);
         tokenId++;
       }
     }
