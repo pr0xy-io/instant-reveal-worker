741774b141237879db6e5054c2d9513615219fc0
gimpey <gimpey+github@pr0xy.io>
2023-07-23 17:45:40 -0400

    streamlined the sleep function

--- src/utils/sleep.ts
+++ src/utils/sleep.ts
@@ -1,10 +1,4 @@
-/**
- * @title Sleep
- * @description Pauses the execution of the program for the specified number of milliseconds.
- *
- * @param {number} ms the number of milliseconds to "sleep" for
- * @returns the promise to be awaited
- */
-export default (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
+/** @description "Pauses" the program for a defined amount of time. */
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export default sleep;
