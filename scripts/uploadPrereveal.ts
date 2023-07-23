af3011e3700bbe67b51b7bc6f76ca9b61ad22859
gimpey <gimpey+github@pr0xy.io>
2023-07-23 17:45:08 -0400

    streamlining the prereveal upload script

--- scripts/uploadPrereveal.ts
+++ scripts/uploadPrereveal.ts
@@ -1,16 +1,22 @@
 import upload from "../src/create/upload";
+import { globSync } from "glob";
 import fs from "fs";
 
-// getting files from the designated prereveal directory and ensuring only the json
-// files are selected, these follow a schema such as 0.json, 1000.json, etc.
-const files = fs.readdirSync("./prereveal");
-const metadatas = files.filter((file) => file.match(/^\d+\.json$/));
-const paths = metadatas.map((metadata) => `./prereveal/${metadata}`);
+/**
+ * @title Upload Prereveal
+ * @author gimpey
+ * @description Retrieves all JSON objects from the designated pre-reveal folder
+ * and uploads them to the Google CDN.
+ */
+const main = async () => {
+  // retrieving all json objects from the prereveal folder
+  const paths = globSync("./prereveal/*.json");
 
-// asynchronously uploading the files to google cdn
-(async () => {
+  // iterates through the json objects and uploads them to the cdn
   for (const path of paths) {
     const metadata = JSON.parse(fs.readFileSync(path).toString());
-    await upload(metadata.tokenId, metadata);
+    await upload(metadata.tokenId || metadata.edition, metadata);
   }
-})();
+};
+
+main();
