129c3be4edd5cfb5c877d438f8c8660aa4347779
gimpey <gimpey+github@pr0xy.io>
2023-07-23 17:46:12 -0400

    refined the constant documentation

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,8 @@
-/** the designated hash for a transfer event */
+/** @description The designated hash for a transfer event. */
 export const TRANSFER_EVENT =
   "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
 
-/** an extended version of the null address (0) */
+/** @description An extended version of the null address (0). */
 export const NULL_ADDRESS =
   "0x0000000000000000000000000000000000000000000000000000000000000000";
 
