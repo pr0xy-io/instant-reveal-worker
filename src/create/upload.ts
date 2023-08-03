2fde4fcecbb0dac0aa3422521e0d8aed1ce2e28b
gimpey <gimpey@pr0xy.io>
2023-08-03 16:02:28 -0400

    converting the upload function to use a config

--- src/create/upload.ts
+++ src/create/upload.ts
@@ -2,16 +2,19 @@ import { Storage } from "@google-cloud/storage";
 import logger from "../services/logger";
 import path from "path";
 
-const subfolder = process.env.NODE_ENV === "production" ? "metadata" : "test";
-const directory = `${process.env.GOOGLE_CLOUD_PROJECT_SLUG}/${subfolder}`;
 const serviceKey = path.resolve(__dirname, "../../keys.json");
+
+const defaultConfig = {
+  bucket: process.env.GOOGLE_CLOUD_BUCKET,
+  slug: process.env.GOOGLE_CLOUD_PROJECT_SLUG,
+  subfolder: process.env.NODE_ENV === "production" ? "metadata" : "test",
+  url: process.env.GOOGLE_CLOUD_URL,
+};
 
 const storage = new Storage({
   keyFilename: serviceKey,
   projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
 });
-
-logger.info(`Writing metadata to Google Cloud Storage Directory: ${directory}`);
 
 /**
  * @title Upload
@@ -20,14 +23,22 @@ logger.info(`Writing metadata to Google Cloud Storage Directory: ${directory}`);
  *
  * @param {number} id the token ID corresponding to the minted NFT
  */
-const upload = async (id: number, metadata: any) => {
+const upload = async (
+  id: number,
+  metadata: any,
+  config: any = defaultConfig
+) => {
+  logger.info(
+    `Writing metadata to Google Cloud Storage Directory: ${config.slug}/${config.subfolder}`
+  );
+
   try {
     // setting up the Google Cloud Storage Bucket and assigning metadata to the
     // uploaded file no-store ensures we can update automatically without
     // needing to clear the cache, while inline (and the `contentType`) ensure
     // you can view the metadata in the browser
-    const bucket = storage.bucket(process.env.GOOGLE_CLOUD_BUCKET);
-    const blob = bucket.file(`${directory}/${id}`);
+    const bucket = storage.bucket(config.bucket);
+    const blob = bucket.file(`${config.slug}/${config.subfolder}/${id}`);
     const blobStream = blob.createWriteStream({
       metadata: {
         contentDisposition: "inline",
@@ -40,7 +51,7 @@ const upload = async (id: number, metadata: any) => {
     // uploads the metadata to Google Cloud; end seems like a weird word choice
     blobStream.end(Buffer.from(JSON.stringify(metadata)));
 
-    const filePath = `${process.env.GOOGLE_CLOUD_URL}/${directory}/${id}`;
+    const filePath = `${config.url}/${config.slug}/${config.subfolder}/${id}`;
     logger.info(`${metadata?.name} uploaded to Google Cloud (${filePath}).`);
   } catch (error) {
     logger.error(`Unable to upload metadata for ${id}: ${error.message}`);
